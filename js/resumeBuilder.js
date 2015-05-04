7f0f2576f4d278ed6b2ada50f6e74b05a8b2a99f
nchristman@expedia.com <earlman@Nates-MacBook-Pro.local>
2015-05-04 16:46:50 -0700

    Updated JS file with objects and JSON data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,94 @@
-var formattedName = HTMLheaderName.replace("%data%", "Nate Christman");
-var formattedRole = HTMLheaderRole.replace("%data%", "Designer");
+var bio = {
+  "name": "Nate Christman",
+  "role": "Designer",
+  "welcomeMessage": "Hi, I'm Nate",
+  "contacts": {
+    "email": "nate.christman@gmail.com",
+    "location": "Seattle, WA".
+    "github": "nchristman",
+    "phone": "(859) 322-9655"
+  },
+  "skills": ["Interaction design", "visual design", "rapid prototyping"]
+}
 
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
+var work = {
+  "jobs": [
+    {
+      "employer": "Expedia",
+      "title": "Sr. Web Designer",
+      "location": "Bellevue, WA",
+      "dates": "06/14 - current",
+      "description": "Product design, interaction design, visual design, rapid prototyping"
+    },
+    {
+      "employer": "projekt202",
+      "title": "UX Designer",
+      "location":"Dallas, TX - Seattle, WA",
+      "dates": "03/12 - 06/14",
+      "description": "Consulted for Expedia and Amazon"
+    },
+    {
+      "employer": "Freelance",
+      "title": "Designer",
+      "location": "Dallas, TX",
+      "dates": "07/04 - current",
+      "description": "Gun for hire"
+    }
+  ]
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "The Creative Circus",
+      "location": "Atlanta, GA",
+      "degree": "Art Direction",
+      "majors": ["Art direction", "Design"],
+      "dates": "09/05 - 10/07",
+      "url": "www.creativecircus.edu"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "title": "Front-end Nanodegree",
+      "school": "Udacity",
+      "dates": "03/15 - current",
+      "url": "www.udacity.com"
+    },
+    {
+      "title": "Front-end Design",
+      "school": "Treehouse",
+      "dates": "04/13 - 09/13",
+      "url": "www.teamtreehouse.com"
+    },
+    {
+      "title": "Front-end Design",
+      "school": "CodeAcademy",
+      "dates": "12/12 - 04/13",
+      "url": "www.codeacademy.com"
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "Expedia Framework",
+      "dates": "06/14 - current",
+      "description": "Design and maintain the framework components and patterns that power Expedia.com and it's other properties and partners",
+      "images": ["http://placekitten.com/g/300/250", "http://placekitten.com/g/300/250", "http://placekitten.com/g/300/250"]
+    },
+    {
+      "title": "Amazon Fire Phone",
+      "dates": "05/14 - 06/14",
+      "description": "Worked with the internal team to design the 'sizzle' site for the Amazon Fire phone",
+      "images": ["http://placekitten.com/g/300/250", "http://placekitten.com/g/300/250", "http://placekitten.com/g/300/250"]
+    },
+    {
+      "title": "Expedia Flights",
+      "dates": "03/12 - 05/14",
+      "description": "Worked on the flights product. Started with fixed width, top/left design, and slowly but surely got the path to a responsive state.",
+      "images": ["http://placekitten.com/g/300/250", "http://placekitten.com/g/300/250", "http://placekitten.com/g/300/250"]
+    }
+  ]
+}
